a434726bad444fc8bec8d2baf3e91147590ae22c
Jannis R <mail@jannisr.de>
2021-11-11 18:02:35 +0100

    encode fake elevators

--- api.js
+++ api.js
@@ -2,9 +2,35 @@
 
 import createCors from 'cors'
 import {createServeFeed} from './lib/serve-feed.js'
+import {encodeFeed, StationUpdate} from './lib/encode-feed.js'
+import {facilitiesSource} from './lib/facilities.js'
+import {logger} from './lib/logger.js'
 
 const cors = createCors()
 const {setFeed, serveFeed} = createServeFeed()
+
+const {OPERATIONAL, CLOSED} = StationUpdate.PathwayStatus
+facilitiesSource.on('data', (facilities) => {
+	const entities = facilities.map((fa, i) => ({
+		id: `e${i}`,
+		station_update: {
+			pathway: [{
+				pathway_id: fa.properties.pathwayId,
+			}],
+			status: fa.properties.isWorking ? OPERATIONAL : CLOSED,
+			// todo: alert_id
+			// todo: direction
+			// todo: elevator metadata as pbf extension
+		},
+	}))
+	const feed = encodeFeed(entities)
+	setFeed(feed)
+
+	logger.debug({
+		nrOfFacilities: facilities.length,
+		feedSize: feed.length,
+	}, 'encoded feed')
+})
 
 export const api = (req, res) => {
 	cors(req, res, (err) => {
