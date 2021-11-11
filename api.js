ec3f0b4de7d9c70fd0c668d75ec42dc4a8e6017f
Jannis R <mail@jannisr.de>
2021-11-11 15:53:39 +0100

    serve feed via HTTP

--- api.js
+++ api.js
@@ -1,8 +1,10 @@
 'use strict'
 
 import createCors from 'cors'
+import {createServeFeed} from './lib/serve-feed.js'
 
 const cors = createCors()
+const {setFeed, serveFeed} = createServeFeed()
 
 export const api = (req, res) => {
 	cors(req, res, (err) => {
@@ -12,6 +14,13 @@ export const api = (req, res) => {
 			return;
 		}
 
-		// todo
+		const path = new URL(req.url, 'http://localhost').pathname
+		// todo: serve explainer page at /
+		if (path === '/feed') {
+			serveFeed(req, res)
+		} else {
+			res.statusCode = 404
+			res.end('nope')
+		}
 	})
 }
