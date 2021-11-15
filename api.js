3ed97c3d4dcb941defa7497f4df60b99f0355fc1
Jannis R <mail@jannisr.de>
2021-11-15 16:51:17 +0100

    optionally merge facilities with foreign feed

--- api.js
+++ api.js
@@ -3,15 +3,20 @@
 import createCors from 'cors'
 import express from 'express'
 import {createServeFeed} from './lib/serve-feed.js'
-import {encodeFeed, StationUpdate} from './lib/encode-feed.js'
+import {encodeFeed, StationUpdate} from './lib/encoding.js'
 import {facilitiesSource} from './lib/facilities.js'
+import {mergeFeedEntitiesWithForeignFeed} from './lib/merge-with-foreign-feed.js'
 import {logger} from './lib/logger.js'
 
 const {setFeed, serveFeed} = createServeFeed()
 
 const {OPERATIONAL, CLOSED} = StationUpdate.PathwayStatus
+
+let entities = []
+let feed = encodeFeed(entities)
+setFeed(feed)
 facilitiesSource.on('data', (facilities) => {
-	const entities = facilities.map((fa, i) => ({
+	entities = facilities.map((fa, i) => ({
 		id: `e${i}`,
 		station_update: {
 			pathway: [{
@@ -23,18 +28,39 @@ facilitiesSource.on('data', (facilities) => {
 			// todo: elevator metadata as pbf extension
 		},
 	}))
-	const feed = encodeFeed(entities)
+	feed = encodeFeed(entities)
 	setFeed(feed)
 
 	logger.debug({
 		nrOfFacilities: facilities.length,
 		feedSize: feed.length,
-	}, 'encoded feed')
+	}, 'updated unmerged feed')
 })
 
 export const api = express()
 api.use(createCors())
 
 // todo: serve explainer page at /
+
+api.use('/feed', async (req, res, next) => {
+	// todo: this allows peeking into our service's local network, prevent this
+	const feedUrl = req.query['mergedWith']
+	if (!feedUrl) return next('route')
+
+	const mergedEntities = await mergeFeedEntitiesWithForeignFeed(entities, feedUrl)
+	const mergedFeed = encodeFeed(mergedEntities)
+	logger.debug({
+		feedUrl,
+		nrOfEntities: mergedEntities.length,
+		feedSize: mergedFeed.length,
+	}, 'merged with foreign feed')
+
+	// This is ugly but sufficient for our mergedWith prototype.
+	const {setFeed, serveFeed} = createServeFeed()
+	setFeed(mergedFeed)
+	serveFeed(req, res, (err) => {
+		if (err) next(err)
+	})
+})
 
 api.use('/feed', serveFeed)
