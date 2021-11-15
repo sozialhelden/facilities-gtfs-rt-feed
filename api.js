3e6a846a475d481fe70a0f0e3713c2e6be01e96d
Jannis R <mail@jannisr.de>
2021-11-15 16:49:24 +0100

    use express

--- api.js
+++ api.js
@@ -1,12 +1,12 @@
 'use strict'
 
 import createCors from 'cors'
+import express from 'express'
 import {createServeFeed} from './lib/serve-feed.js'
 import {encodeFeed, StationUpdate} from './lib/encode-feed.js'
 import {facilitiesSource} from './lib/facilities.js'
 import {logger} from './lib/logger.js'
 
-const cors = createCors()
 const {setFeed, serveFeed} = createServeFeed()
 
 const {OPERATIONAL, CLOSED} = StationUpdate.PathwayStatus
@@ -32,21 +32,9 @@ facilitiesSource.on('data', (facilities) => {
 	}, 'encoded feed')
 })
 
-export const api = (req, res) => {
-	cors(req, res, (err) => {
-		if (err) {
-			res.statusCode = err.statusCode || 500
-			res.end(err + '')
-			return;
-		}
+export const api = express()
+api.use(createCors())
 
-		const path = new URL(req.url, 'http://localhost').pathname
-		// todo: serve explainer page at /
-		if (path === '/feed') {
-			serveFeed(req, res)
-		} else {
-			res.statusCode = 404
-			res.end('nope')
-		}
-	})
-}
+// todo: serve explainer page at /
+
+api.use('/feed', serveFeed)
